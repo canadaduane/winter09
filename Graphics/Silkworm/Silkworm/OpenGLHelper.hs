d3603560180335f6cbcb8cc48e70b76122c68c0a
Duane Johnson <duane.johnson@gmail.com>
2009-03-07 11:37:24 -0700

    Rotating tunnel is working

--- Graphics/Silkworm/Silkworm/OpenGLHelper.hs
+++ Graphics/Silkworm/Silkworm/OpenGLHelper.hs
@@ -7,25 +7,66 @@ module Silkworm.OpenGLHelper where
   initOpenGL :: IO ()
   initOpenGL = do
     -- Use some defaults for drawing with antialiased edges
-    clearColor  $= Color4 0.3 0.3 0.3 1.0
-    pointSmooth $= Enabled
-    pointSize   $= 3
-    lineSmooth  $= Enabled
-    lineWidth   $= 2.5
+    clearColor  $= Color4 0.3 0.3 0.3 0.0
+    -- pointSmooth $= Enabled
+    -- pointSize   $= 3
+    -- lineSmooth  $= Enabled
+    -- lineWidth   $= 2.5
     blend       $= Enabled
     blendFunc   $= (SrcAlpha, OneMinusSrcAlpha)
+    shadeModel  $= Smooth
+    normalize   $= Enabled
+    polygonMode $= (Fill, Fill)
     
     -- Set up the viewport
+    viewport    $= (Position 0 0, Size w h)
     matrixMode  $= Projection
     loadIdentity
-    ortho x1 x2 y1 y2 (-1) 1
-    translate (Vector3 0.5 0.5 0 :: Vector3 GLfloat)
+    ortho (-1) 1 (-1) 1 (-1) 1
+    perspective 90.0 (fw/2/(fh/2)) 0.0 20.0
+    -- translate (Vector3 0.5 0.5 0 :: Vector3 GLfloat)
+    flush
+    
+    lighting           $= Enabled
+    -- position (Light 0) $= Vertex4 0.0 0.0 1.0 0.0
+    -- ambient  (Light 0) $= Color4  0.5 0.5 0.5 1.0
+    -- light    (Light 0) $= Enabled 
+    
+    position (Light 1) $= Vertex4 0.0 0.0 (0.5) 0.0
+    ambient  (Light 1) $= Color4  0.2 0.2 0.2 1.0
+    diffuse  (Light 1) $= Color4  1.0 1.0 1.0 1.0
+    specular (Light 1) $= Color4  0.0 0.0 0.0 1.0
+    light    (Light 1) $= Enabled
+    
+    -- materialSpecular  FrontAndBack $= Color4 1.0 1.0 1.0 1.0
+    -- materialEmission  FrontAndBack $= Color4 0.0 0.0 0.0 1.0
+    -- materialShininess FrontAndBack $= 70.0
+    colorMaterial $= Just (Front, Diffuse)
+    
+    -- matrixMode  $= Projection
+    -- loadIdentity
+    -- ortho (-1) 1 (-1) 1 (-1) 1
+    -- translate (Vector3 0.5 0.5 0 :: Vector3 GLfloat)
+    -- 
+    -- materialSpecular Front $= Color4 1 1 1 1
+    -- materialShininess Front $= 50
+    -- position (Light 0) $= Vertex4 1 1 0 1
+    -- 
+    -- lighting $= Enabled
+    -- light (Light 0) $= Enabled
+    -- depthFunc $= Just Less
+    
+    matrixMode $= Modelview 0
+    loadIdentity
+    flush
     
     return ()
     
     where
-      x1 = -(fromIntegral (fst windowDimensions)) / 2
-      x2 =  (fromIntegral (fst windowDimensions)) / 2
-      y1 = -(fromIntegral (snd windowDimensions)) / 2
-      y2 =  (fromIntegral (snd windowDimensions)) / 2
+      (w, h) = windowDimensions
+      (fw, fh) = (fromIntegral w, fromIntegral h) :: (GLdouble, GLdouble)
+      x1 = -fw / 2
+      x2 =  fw / 2
+      y1 = -fh / 2
+      y2 =  fh / 2
   
--- Graphics/Silkworm/Silkworm/Title.hs
+++ Graphics/Silkworm/Silkworm/Title.hs
@@ -17,8 +17,11 @@ module Silkworm.Title where
     Vector3(..), scale, translate, rotate,
     Vertex3(..), vertex,
     Color3(..), color,
+    Normal3(..), normal,
     PrimitiveMode(..), renderPrimitive,
-    preservingMatrix)
+    MatrixMode(..), matrixMode, loadIdentity,
+    preservingMatrix,
+    ($=))
   import Graphics.Rendering.OpenGL.GL.StateVar (get)
   import Graphics.Rendering.FTGL (
     RenderMode(..), Font, createTextureFont, createPolygonFont,
@@ -41,20 +44,49 @@ module Silkworm.Title where
   
   data TitleState = TitleState {
     tsObjects :: [TitleObject],
+    tsAngle   :: Float,
     tsFont    :: Font
   }
   
   rasterRect = ((0, 0), (200, 200))
-  testTunnel = rasterizeLines [((50,50), (100,130)), ((0,100), (200,100))] rasterRect 20.0
+  testTunnel = rasterizeLines [((50,50), (100,130)), ((50,100), (150,100))] rasterRect 20.0
   
-  drawTunnel :: Array.Array (Int, Int) Float -> IO ()
-  drawTunnel t = preservingMatrix $ do
-    translate (Vector3 (-100) (-100) (0.0 :: GLfloat))
-    let point = renderPrimitive Points . vertex in
-      forM_ (Array.assocs t) $ \((x, y), d) -> do
-        color $ Color3 (d / 20) 0.0 (0.0 :: GLfloat)
-        point $ Vertex3 (fromIntegral x) (fromIntegral y) (0.0 :: GLfloat)
-    
+  cross :: (Float, Float, Float) -> (Float, Float, Float) -> (Float, Float, Float)
+  -- cross (x0, y0, z0) (x1, y1, z1) = ((y0*z1 - z0*y1), (z0*x1 - x0*z1), (x0*y1 - x1*y0))
+  cross (a1, a2, a3) (b1, b2, b3) = ((a2*b3 - a3*b2), (a3*b1 - a1*b3), (a1*b2 - a2*b1))
+  
+  drawTunnel :: Array.Array (Int, Int) Float -> Float -> IO ()
+  drawTunnel t angle = preservingMatrix $ do
+    rotate angle $ Vector3 1 0.5 (0 :: GLfloat)
+    scale 2.0 2.0 (2.0 :: Float)
+    translate (Vector3 (-0.5) (-0.5) (0.0 :: GLfloat))
+    color $ Color3 0.5 0.5 (1.0 :: GLfloat) 
+    let ((x1, y1), (x2, y2)) = bounds t
+        rng = range ((x1, y1), (x2 - 1, y2 - 1))
+        shrink n = (fromIntegral n) / 200
+        -- v x y z = vertex (Vertex3 (shrink x) (shrink y) z / 200.0)
+        xyz x y = (shrink x, shrink y, (t ! (x, y)) / 200.0)
+      in
+      forM_ rng $ \(x, y) -> renderPrimitive Quads $ do
+        let (x0, y0, z0) = xyz x y
+            (x1, y1, z1) = xyz (x + 1) y
+            (x2, y2, z2) = xyz x (y + 1)
+            (x3, y3, z3) = xyz (x + 1) (y + 1)
+            (nx, ny, nz) = cross (x1 - x0, y1 - y0, z1 - z0) (x2 - x0, y2 - y0, z2 - z0)
+        normal $ Normal3 nx ny nz
+        vertex $ Vertex3 x0 y0 z0
+        vertex $ Vertex3 x1 y1 z1
+        vertex $ Vertex3 x3 y3 z3
+        vertex $ Vertex3 x2 y2 z2
+        
+        -- vertex $ Vertex3 1.0 1.0 (1.0 :: Float)
+        -- let xf = fromIntegral x
+        --     yf = fromIntegral y
+        --   in vertex $ Vertex3 (xf :: GLfloat) yf (t ! (x, y))
+        --      vertex $ Vertex3 (xf + 1) yf (t ! (x + 1, y))
+        --      vertex $ Vertex3 (xf + 1) (yf + 1) (t ! (x + 1, y + 1))
+        --      vertex $ Vertex3 xf (yf + 1) (t ! (x, y + 1))
+  
   -- | Return a TitleState object set to reasonable initial values
   newTitleState :: IO TitleState
   newTitleState = do
@@ -64,21 +96,21 @@ module Silkworm.Title where
     font <- createPolygonFont "scribbled.ttf"
     -- putStrLn (show $ getFontError font)
     -- exitWith (ExitFailure (-1))
-    return (TitleState objs font)
+    return (TitleState objs 0.0 font)
   
   showTitleScreen :: IO ()
   showTitleScreen = do
     loadResources
-    state <- newTitleState
+    state <- newTitleState >>= newIORef
     titleScreenLoop state
   
-  titleScreenLoop :: TitleState -> IO ()
-  titleScreenLoop state = do
+  titleScreenLoop :: IORef TitleState -> IO ()
+  titleScreenLoop stateVar = do
     keySet <- getPressedKeys [SpecialKey ESC]
-    updateDisplay state
+    updateDisplay stateVar
     if keyIsPressed (SpecialKey ESC) keySet
       then return ()
-      else titleScreenLoop state
+      else titleScreenLoop stateVar
   
   -- | Load title screen textures into OpenGL buffers
   loadResources :: IO ()
@@ -87,37 +119,43 @@ module Silkworm.Title where
     return ()
     
   -- | Renders the current state.
-  updateDisplay :: TitleState -> IO ()
-  updateDisplay state = do
+  updateDisplay :: IORef TitleState -> IO ()
+  updateDisplay stateVar = do
     clear [ColorBuffer]
-    drawTitle state
+    drawTitle stateVar
     -- when (slowKey == Press) drawSlowMotion
     -- forM_ (M.assocs $ stShapes state) (fst . snd) -- Draw each one
     swapBuffers
   
   -- | Draw the Silkworm title screen
-  drawTitle :: TitleState -> IO ()
-  drawTitle state = preservingMatrix $ do
+  drawTitle :: IORef TitleState -> IO ()
+  drawTitle stateVar = preservingMatrix $ do
+    -- matrixMode $= Modelview 0
+    -- loadIdentity
     -- forM_ (tsObjects state) $ \(TitleObject {tImage = image}) -> do
     --   
-    renderTexture 0 (-400) (-300) 800 600
+    -- renderTexture 0 (-400) (-300) 800 600
+    state <- get stateVar
+    stateVar $= state { tsAngle = ((tsAngle state) + 5.0) }
+    drawTunnel testTunnel (tsAngle state)
     
-    seed <- newStdGen
-    let (s1, s2) = split seed
-        bump1 = (randomBumpyRaster rasterRect 40 10.0 s1)
-        bump2 = (randomBumpyRaster rasterRect 20 20.0 s2)
-        composite = ((testTunnel #+ 1) #*# (bump1 #* 0.5) #*# (bump2 #* 0.5))
-      in drawTunnel testTunnel
+    -- seed <- newStdGen
+    -- let (s1, s2) = split seed
+    --     bump1 = (randomBumpyRaster rasterRect 40 10.0 s1)
+    --     bump2 = (randomBumpyRaster rasterRect 20 20.0 s2)
+    --     composite = ((testTunnel #+ 1) #*# (bump1 #* 0.5) #*# (bump2 #* 0.5))
+    --   in drawTunnel testTunnel
+      
     -- let font = (tsFont state)
     -- font <- createTextureFont "shrewsbury.ttf"
     -- setFontFaceSize font 72 72
     -- renderFont font "Silkworm!" All
     
     -- scale 3.75 3.75 (1.0 :: GLfloat)
-    -- let render str = do
-    --       translate (Vector3 0.0 (-16) (0.0 :: GLfloat))
-    --       renderString Fixed8x16 str
-    -- 
-    -- color $ Color3 1.0 1.0 (1.0 :: GLfloat)
-    -- render "Silkworm!"
+    let render str = do
+          translate (Vector3 0.0 0.0 (0.0 :: GLfloat))
+          renderString Fixed8x16 str
+    
+    color $ Color3 1.0 1.0 (1.0 :: GLfloat)
+    render "Silkworm!"
     
