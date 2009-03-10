3206608c391ecc9cde8641cdd487417c36fcf32e
Duane Johnson <duane.johnson@gmail.com>
2009-03-09 19:23:47 -0600

    Cleanup main code

--- Parallel Computing/Lab5/pquicksort.c
+++ Parallel Computing/Lab5/pquicksort.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <mpi.h>
 
 #include "misc.h"
@@ -56,47 +57,51 @@ void show_div(Div d)
 
 int main(int argc, char *argv[])
 {
-    int size_param;
-    int iter = 0;
-    double start = 0.0, finish = 0.0;
-    size_param = shell_arg_int(argc, argv, "--size", 8);
-    int size = 1 << (size_param - 1);
-    
     MPI_Init(&argc, &argv);
     
     MPI_Comm_size(MPI_COMM_WORLD, &nproc);
     MPI_Comm_rank(MPI_COMM_WORLD, &iproc);
     
-    if (iproc == 0)
-        fprintf( stderr, "Sorting %d numbers total...\n", nproc * size);
-    
     // Seed the random number generator with our processor ID
     srandom(iproc);
     
-    printf("Node %d started...\n", iproc);
-    
-    start = when();
-    
     {
-        MPI_Status status;
-        int *numbers;
+        double start = 0.0, finish = 0.0;
+        int size_param = shell_arg_int(argc, argv, "--size", 8);
+        int iter = 0, dim = (int) ceil(sqrt( (double) nproc ));
+        int all_size = 1 << (size_param - 1);
+        int *all_numbers = alloc_n_random( all_size );
         
-        numbers = alloc_n_random( size );
-        fprintf(stderr, "Node %d has %d random numbers.\n", iproc, size);
+        if (iproc == 0) fprintf( stderr, "Sorting %d numbers total...\n", nproc * all_size);
         
-        // fprintf(stderr, "Node %d numbers: ", iproc);
-        // for (iter = 0; iter < size; iter++) fprintf(stderr, "%d, ", numbers[iter]);
-        fprintf(stderr, "Node %d's median: %d\n", iproc, median(numbers, size));
+        printf("Node %d started...\n", iproc);
         
+        start = when();
+        {
+            MPI_Status status;
+            MPI_Comm new_comm;
+            int* numbers = all_numbers;
+            int size = all_size;
+            int median;
+            
+            fprintf(stderr, "Node %d has %d random numbers.\n", iproc, all_size);
+            
+            for ( iter = 0; iter < dim; iter++ )
+            {
+                median = median_of_three( numbers, size );
+            }
+            divided = divide(numbers, size, )
+            other = iproc & 0x01
+            MPI_Comm_split(MPI_COMM_WORLD, color, 0, &new_comm);
+        }
+        finish = when();
         
+        // All nodes:
+        printf("Node %d completed %d iterations in %f seconds.\n", iproc, iter, finish - start);
     }
-    
-    finish = when();
+
     
     MPI_Finalize();
-    
-    // All nodes:
-    printf("Node %d completed %d iterations in %f seconds.\n", iproc, iter, finish - start);
     
     return 0;
 }
