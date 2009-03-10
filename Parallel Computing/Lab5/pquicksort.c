72514931288f1a44bd214c40b0277abdaf8fb5af
Duane Johnson <duane.johnson@gmail.com>
2009-03-09 19:53:07 -0600

    Adding reduce code

--- Parallel Computing/Lab5/pquicksort.c
+++ Parallel Computing/Lab5/pquicksort.c
@@ -7,6 +7,9 @@
 #include "misc.h"
 #include "median.h"
 
+#define MSG_SIZE    0
+#define MSG_NUMBERS 1
+
 typedef struct DIV {
     int* less_than;
     int less_size;
@@ -55,6 +58,47 @@ void show_div(Div d)
     printf ("\n");
 }
 
+void reduce( int nproc, int iproc, int* numbers, int size )
+{
+    MPI_Request request;
+    MPI_Status status;
+    MPI_Comm new_comm;
+    int dim = (int) ceil(sqrt( (double) nproc ));
+    int median;
+    int i;
+    
+    // Allocate buffer for receiving values
+    int tmp_max_size = size;
+    int tmp_size = 0;
+    int* tmp_numbers = calloc( tmp_max_size, sizeof(int) );
+    
+    fprintf(stderr, "Node %d has %d random numbers.\n", iproc, all_size);
+    
+    for ( i = 0; i < dim; i++ )
+    {
+        int dest = iproc ^ (1 << i);
+        
+        median = median_of_three( numbers, size );
+        divided = divide( numbers, size, median );
+        
+        MPI_Isend(&size,      sizeof(int), MPI_INT,
+                  dest, MSG_SIZE,    MPI_COMM_WORLD, &request);
+        MPI_Isend(numbers,    sizeof(int), MPI_INT,
+                  dest, MSG_NUMBERS, MPI_COMM_WORLD, &request);
+        
+        MPI_Recv(&tmp_size,   sizeof(int), MPI_INT,
+                 dest, MSG_SIZE,    MPI_COMM_WORLD, &status);
+        if (tmp_size > tmp_max_size) 
+            tmp_numbers = realloc( tmp_numbers, tmp_size * sizeof(int) );
+        MPI_Recv(tmp_numbers, sizeof(int), MPI_INT,
+                 dest, MSG_NUMBERS, MPI_COMM_WORLD, &status);
+        
+        // other = iproc & 0x01
+        // MPI_Comm_split(MPI_COMM_WORLD, color, 0, &new_comm);
+    }
+
+}
+
 int main(int argc, char *argv[])
 {
     MPI_Init(&argc, &argv);
@@ -67,9 +111,7 @@ int main(int argc, char *argv[])
     
     {
         double start = 0.0, finish = 0.0;
-        int size_param = shell_arg_int(argc, argv, "--size", 8);
-        int iter = 0, dim = (int) ceil(sqrt( (double) nproc ));
-        int all_size = 1 << (size_param - 1);
+        int all_size = shell_arg_int(argc, argv, "--size", 16);
         int *all_numbers = alloc_n_random( all_size );
         
         if (iproc == 0) fprintf( stderr, "Sorting %d numbers total...\n", nproc * all_size);
@@ -77,23 +119,7 @@ int main(int argc, char *argv[])
         printf("Node %d started...\n", iproc);
         
         start = when();
-        {
-            MPI_Status status;
-            MPI_Comm new_comm;
-            int* numbers = all_numbers;
-            int size = all_size;
-            int median;
-            
-            fprintf(stderr, "Node %d has %d random numbers.\n", iproc, all_size);
-            
-            for ( iter = 0; iter < dim; iter++ )
-            {
-                median = median_of_three( numbers, size );
-            }
-            divided = divide(numbers, size, )
-            other = iproc & 0x01
-            MPI_Comm_split(MPI_COMM_WORLD, color, 0, &new_comm);
-        }
+        reduce( nproc, iproc, all_numbers, all_size );
         finish = when();
         
         // All nodes:
