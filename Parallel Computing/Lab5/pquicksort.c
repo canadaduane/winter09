b0067470bcc9671c46c9e19f3ac5f0ea67ada587
Duane Johnson <duane.johnson@gmail.com>
2009-03-10 12:21:54 -0600

    Parallel quicksort working, although the final numbering of nodes is weird

--- Parallel Computing/Lab5/pquicksort.c
+++ Parallel Computing/Lab5/pquicksort.c
@@ -12,9 +12,9 @@
 
 typedef struct DIV {
     int median;
-    int* less_than;
-    int less_size;
-    int* greater_than;
+    int* lesser;
+    int lesser_size;
+    int* greater;
     int greater_size;
 } Div;
 
@@ -41,9 +41,9 @@ Div divide(int* numbers, int size, int median)
             break;
         }
     }
-    d.less_than = numbers;
-    d.less_size = i;
-    d.greater_than = numbers + i;
+    d.lesser = numbers;
+    d.lesser_size = i;
+    d.greater = numbers + i;
     d.greater_size = size - i;
     return d;
 }
@@ -52,18 +52,19 @@ void show_div(int iproc, Div d)
 {
     int i;
     fprintf(stderr, "Division for iproc %d:", iproc);
-    fprintf(stderr, "\n\tLess than %d (%d): ", d.median, d.less_size);
-    for( i = 0; i < d.less_size; i++) fprintf(stderr, "%d, ", d.less_than[i]);
+    fprintf(stderr, "\n\tLess than %d (%d): ", d.median, d.lesser_size);
+    for( i = 0; i < d.lesser_size; i++) fprintf(stderr, "%d, ", d.lesser[i]);
     fprintf(stderr, "\n\tGreater than %d (%d): ", d.median, d.greater_size);
-    for( i = 0; i < d.greater_size; i++) fprintf(stderr, "%d, ", d.greater_than[i]);
+    for( i = 0; i < d.greater_size; i++) fprintf(stderr, "%d, ", d.greater[i]);
     fprintf (stderr, "\n");
 }
 
-void hypercube( int nproc, int iproc, int* original_numbers, int original_size )
+void hypercube( int nproc, int original_iproc, int* original_numbers, int original_size )
 {
     MPI_Request request;
     MPI_Status status;
     MPI_Comm comm = MPI_COMM_WORLD;
+    int iproc = original_iproc;
     // Number of dimensions e.g. 
     int dim = floor_log2( nproc );
     // The allocated space of our original_numbers array (grows as needed)
@@ -89,35 +90,38 @@ void hypercube( int nproc, int iproc, int* original_numbers, int original_size )
         median = median_of_three( numbers, size );
         MPI_Bcast (&median, 1, MPI_INT, 0, comm);
         
-        fprintf(stderr, "Node %d dividing %d numbers using median %d...\n", iproc, size, median);
+        // fprintf(stderr, "Node %d dividing %d numbers using median %d...\n", iproc, size, median);
         divided = divide( numbers, size, median );
-        show_div(iproc, divided);
+        // show_div(iproc, divided);
         
         if (iproc % 2 == 0)
         {
-            offset  = (int)(divided.less_than - original_numbers);
-            size    = divided.less_size;
-            numbers = divided.less_than;
+            offset  = (int)(divided.lesser - original_numbers);
+            size    = divided.lesser_size;
+            numbers = divided.lesser;
+            // fprintf(stderr, "Node %d sending to %d, size %d (greater)\n", iproc, dest, divided.greater_size);
             MPI_Isend(&(divided.greater_size), 1, MPI_INT,
                       dest, MSG_SIZE,    comm, &request);
-            MPI_Isend(  divided.greater_than,  divided.greater_size, MPI_INT,
+            MPI_Isend(  divided.greater,  divided.greater_size, MPI_INT,
                       dest, MSG_NUMBERS, comm, &request);
         }
         else
         {
-            offset  = (int)(divided.greater_than - original_numbers);
+            offset  = (int)(divided.greater - original_numbers);
             size    = divided.greater_size;
-            numbers = divided.greater_than;
-            MPI_Isend(&(divided.less_size), 1, MPI_INT,
+            numbers = divided.greater;
+            // fprintf(stderr, "Node %d sending to %d, size %d (lesser)\n", iproc, dest, divided.lesser_size);
+            MPI_Isend(&(divided.lesser_size), 1, MPI_INT,
                       dest, MSG_SIZE,    comm, &request);
-            MPI_Isend(  divided.less_than,  divided.less_size, MPI_INT,
+            MPI_Isend(  divided.lesser,  divided.lesser_size, MPI_INT,
                       dest, MSG_NUMBERS, comm, &request);
         }
         
+        // fprintf(stderr, "Node %d receiving from %d\n", iproc, dest);
         MPI_Recv(&tmp_size,      1, MPI_INT,
                  dest, MSG_SIZE,    comm, &status);
-        if (tmp_size > max_size)
-        {
+        // if (tmp_size > max_size)
+        // {
             int* new_pointer =
                 realloc( original_numbers,
                         (offset + size + tmp_size) * sizeof(int) );
@@ -126,7 +130,7 @@ void hypercube( int nproc, int iproc, int* original_numbers, int original_size )
                 numbers = new_pointer + offset;
                 original_numbers = new_pointer;
             }
-        }
+        // }
         MPI_Recv(numbers + size, tmp_size, MPI_INT,
                  dest, MSG_NUMBERS, comm, &status);
         size += tmp_size;
@@ -135,7 +139,9 @@ void hypercube( int nproc, int iproc, int* original_numbers, int original_size )
         MPI_Comm_split(comm, iproc % 2, iproc >> 1, &comm);
         iproc = iproc >> 1;
     }
-
+    
+    fprintf(stderr, "Final numbers for node %d: (%d)\n\t", original_iproc, size);
+    for( i = 0; i < size; i++) fprintf(stderr, "%d, ", numbers[i]);
 }
 
 int main(int argc, char *argv[])
@@ -158,9 +164,9 @@ int main(int argc, char *argv[])
         {
             fprintf( stderr, "Sorting %d numbers total...\n", nproc * all_size);
 
-            fprintf(stderr, "Node %d started...\n\t", iproc);
-            for (i = 0; i < all_size; i++) fprintf(stderr, "%d, ", all_numbers[i]);
-            fprintf(stderr, "\n");
+            fprintf(stderr, "Node %d started...\n", iproc);
+            // for (i = 0; i < all_size; i++) fprintf(stderr, "%d, ", all_numbers[i]);
+            // fprintf(stderr, "\n");
         }
         
         start = when();
